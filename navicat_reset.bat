6cda76ef163104601439845baab993d87bb1af92
Alfred <alfredleo@gmail.com>
2024-05-02 18:48:41 +0500

    working solutions for windows are manual in navicat_reset.bat and reset_navicat_52pojie.sh for mac. Tested on latest 16.3.8 Navicat Premium

--- navicat_reset.bat
+++ navicat_reset.bat
@@ -1,2 +1,29 @@
+REM MANUAL ---
+REM 1) Navicat off
+REM Uninstall it with uninstall tool.
 
+REM 2) Win + R, enter regedit, enter
+REM Delete HKEY_CURRENT_USER\Software\PremiumSoft\Data
+REM Expand HKEY_CURRENT_USER\Software\Classes\CLSID
+REM Expand each subfolder. If it contains only one folder named Info, delete it
 
+REM AUTO ---
+REM This is cmd version but did not work for me. Remove by hand worked. You are welcome to develop and test it if you find errors.
+@echo off
+
+echo Delete HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium\Registration15XEN
+echo waiting......
+reg delete "HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium\Registration15XEN" /va /f
+echo.
+
+echo Delete Info folder under HKEY_CURRENT_USER\Software\Classes\CLSID
+echo waiting......
+
+for /f %%i in ('"REG QUERY "HKEY_CURRENT_USER\Software\Classes\CLSID" /s | findstr /E Info"') do (
+    reg delete %%i /va /f
+)
+
+echo.
+echo Finish
+
+pause
